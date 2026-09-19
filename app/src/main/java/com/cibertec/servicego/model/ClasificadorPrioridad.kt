03f047018875518ec19d58665e92aca2c9934cc7
msejuro312 <msejuro312@protonmail.com>
2026-09-19 16:54:04 -0500

    segundo commit 19.09

--- app/src/main/java/com/cibertec/servicego/model/ClasificadorPrioridad.kt
+++ app/src/main/java/com/cibertec/servicego/model/ClasificadorPrioridad.kt
@@ -3,4 +3,28 @@ package com.cibertec.servicego.model
 // COMIENZA CAMBIO PARA EL CHECKPOINT 02.
 // Esta clase se completará con una función que utilizará when para determinar
 // la prioridad según las condiciones operativas del servicio.
-class ClasificadorPrioridad
+class ClasificadorPrioridad {
+    fun determinarPrioridad(
+        descripcion: String,
+        modalidad: String,
+        modalidadPrioritaria: String,
+        prioridadAlta: String,
+        prioridadMedia: String,
+        prioridadBaja: String,
+    ): String {
+        val descripcionNormalizada = descripcion.lowercase()
+        val presentaFallaOperativa =
+            "falla" in descripcionNormalizada ||
+                    "error" in descripcionNormalizada ||
+                    "intermitente" in descripcionNormalizada ||
+                    "lento" in descripcionNormalizada ||
+                    "atasco" in descripcionNormalizada
+
+            return when {
+                modalidad == modalidadPrioritaria -> prioridadAlta
+                presentaFallaOperativa -> prioridadMedia
+                else -> prioridadBaja
+            }
+    }
+
+}
--- app/src/main/java/com/cibertec/servicego/model/ServicioFactory.kt
+++ app/src/main/java/com/cibertec/servicego/model/ServicioFactory.kt
@@ -2,4 +2,25 @@ package com.cibertec.servicego.model
 
 // COMIENZA CAMBIO PARA EL CHECKPOINT 02.
 // Este object se completará con una función encargada de crear objetos Servicio.
-object ServicioFactory
+object ServicioFactory {
+
+    fun crearServicio (
+        cliente: String,
+        descripcion: String,
+        direccion: String,
+        codigo: String,
+        estado: String,
+        prioridad: String,
+        rutaAtencion: String
+    ): Servicio {
+        return Servicio(
+            cliente = cliente,
+            descripcion  = descripcion,
+            direccion = direccion,
+            codigo = codigo,
+            estado = estado,
+            prioridad = prioridad,
+            rutaAtencion = rutaAtencion
+        )
+    }
+}
